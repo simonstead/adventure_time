a6e8dd9d3803ae2b130b8897bf96d5609019dec3
Simon Stead <simonstead@me.com>
2017-04-09 20:09:18 +0100

    Add header buttons, to become three separate pages

--- index.ios.js
+++ index.ios.js
@@ -20,9 +20,13 @@ import Finn from './components/finn';
 export default class adventure_time extends Component {
   constructor(props) {
     super(props)
-    this.state = { finnHeight: 1 }
+    this.state = {
+      finnHeight: 1,
+      active_page: 0
+    }
     this.onPress = this.press_me.bind(this);
     this.reset = this.reset.bind(this);
+    this.pressHeaderButton = this.pressHeaderButton.bind(this);
   }
   press_me = () => {
     console.log(this.state.finnHeight);
@@ -30,6 +34,10 @@ export default class adventure_time extends Component {
     console.log(this.state.finnHeight);
   };
   reset = () => this.setState({ finnHeight: 1 });
+  pressHeaderButton = (active_page) => {
+    console.log(active_page);
+    this.setState({active_page: active_page})
+  }
   render() {
     return (
       // <View style={styles.container}>
@@ -46,9 +54,14 @@ export default class adventure_time extends Component {
       //   <Finn></Finn>
       // </View>
       <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 1, backgroundColor: 'powderblue', flexDirection: "row", justifyContent: 'center', paddingTop: '5%'}}>
+          <Button title="Page 1" onPress={() => this.pressHeaderButton('PAGE_ONE')}></Button>
+          <Button title="Page 2" onPress={() => this.pressHeaderButton('PAGE_TWO')}></Button>
+          <Button title="Page 3" onPress={() => this.pressHeaderButton('PAGE_THREE')}></Button>
+        </View>
         <View style={{flex: 1, backgroundColor: 'skyblue'}} >
           <Text>{this.state.finnHeight}</Text>
+          <Text>{this.state.active_page}</Text>
         </View>
         <View style={{flex: this.state.finnHeight, backgroundColor: 'steelblue'}}>
         <TouchableHighlight onPress={this.onPress}>
